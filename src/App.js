186e9fa47bf55710057cfc31a5f9d2641c9ac547
Michel <michelvanderlinden@live.nl>
2021-05-04 18:14:41 +0200

    Navigation bar now also set the page using states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ProjectElementComponent from './ProjectElementComponent'
-import NavigationBarElement from './NavigationBarElement'
+import NavigationBarElement from './NavigationBarComponent'
 import FooterElement from './FooterElement'
 import "./style.css"
 import {ProjectsData} from "./ProjectsData"
@@ -21,8 +21,13 @@ class App extends Component
 
   LoadProjectPage=(selectedProjectInfo)=>
   {
-    this.setState({currentPage:"ProjectPage"})
+    this.SwitchPage("ProjectPage")
     this.setState({currentSelectedProject:selectedProjectInfo});
+  }
+
+  SwitchPage=(newPage)=>
+  {
+    this.setState({currentPage:newPage})
   }
 
   render()
@@ -31,7 +36,7 @@ class App extends Component
     {
       return (
         <div>
-          <NavigationBarElement/>
+          <NavigationBarElement pageSwitcher={this.SwitchPage}/>
           <h1>Projects</h1>
           <div className="ProjectItemsContainer">
     
@@ -50,13 +55,24 @@ class App extends Component
     {
       return (
         <div>
-          <NavigationBarElement/>
+          <NavigationBarElement pageSwitcher={this.SwitchPage}/>
           <div className="ProjectPage">
             <h1>{this.state.currentSelectedProject.projectTitle}</h1>
             <img className="ProjectPageImage" src={this.state.currentSelectedProject.projectDirectory+"logo.jpg"}/>
             <h1>Project Description</h1>
             <p className="ProjectPageDesciption">{this.state.currentSelectedProject.projectDescription}</p>
           </div>
+          <FooterElement/>
+        </div>
+        
+      );
+    }
+    else if(this.state.currentPage=="AboutMe")
+    {
+      return (
+        <div>
+          <NavigationBarElement pageSwitcher={this.SwitchPage}/>
+            <h1>About Me</h1>
           <FooterElement/>
         </div>
         
---
+++ src/NavigationBarComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+
+class NavigationBarComponent extends Component
+{
+
+  constructor(props)
+  {
+      super(props);
+  }
+
+  render()
+  {
+    return(
+        <nav>
+        <a onClick={this.OnProjectsClick}>Projects</a>
+        <a onClick={this.OnAboutMeClick}>About Me</a>
+        </nav>    
+    );
+  }
+
+  OnProjectsClick=()=>
+  {
+    this.props.pageSwitcher("ProjectsGallery");
+  }
+
+  OnAboutMeClick=()=>
+  {
+    this.props.pageSwitcher("AboutMe");
+  }
+}
+
+export default NavigationBarComponent;

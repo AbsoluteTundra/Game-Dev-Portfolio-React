270719fcbaeb3daa7aa65cc9c86398fcf54ea40d
AbsoluteTundra <michelvanderlinden@live.nl>
2023-03-10 11:34:57 +0100

    updated wip about me page

--- src/Pages/AboutMePage.js
+++ src/Pages/AboutMePage.js
@@ -13,9 +13,45 @@ class AboutMePage extends Component {
         <NavigationBarElement pageSwitcher={this.props.pageSwitcher} />
         <h1>About Me</h1>
         <div className="AboutMeContainer">
-          <img src="https://media.licdn.com/dms/image/C5603AQFwk5vLwsEW5w/profile-displayphoto-shrink_800_800/0/1570112065950?e=1676505600&v=beta&t=lTlXMIjwN2QN-aoM0EjczU66M0d2_Fp4SMtgSpuBGpc" alt=""></img>
-          <p>Hello my name is Michel. I'm gameplay programmer </p>
+          <img src="https://media.licdn.com/dms/image/C5603AQFwk5vLwsEW5w/profile-displayphoto-shrink_800_800/0/1570112065950?e=1683763200&v=beta&t=tVY6zjw4oiFh87i0kmh0T0UhipZCvZho2nACTw6K7ug" alt=""></img>
+          <div>
+            <p>I am a gameplay and tools programmer with expertise in Unity and Unreal engines. I love creating engaging game mechanics and improving development workflows through efficient tools. My passion for game development drives me to learn and grow, and I'm always looking for new challenges.</p>
+          </div>
         </div>
+
+        <h1>Experiences</h1>
+        <div className='ExperienceBlock'>
+          <img src="https://media.licdn.com/dms/image/C510BAQHRl2VJ0_8qZw/company-logo_100_100/0/1519875113257?e=1686787200&v=beta&t=l-pb48QF9g_82IgrxVsWqBD7VlJnu5Cx7ajpkbtgVik"></img>
+          <div>
+            <h1>Unity Software-Engineer 2021 - Current</h1>
+            <p>Working on simulations @BlueTea</p>
+          </div>
+        </div>
+
+        <div className='ExperienceBlock'>
+          <img src="https://media.licdn.com/dms/image/C510BAQHRl2VJ0_8qZw/company-logo_100_100/0/1519875113257?e=1686787200&v=beta&t=l-pb48QF9g_82IgrxVsWqBD7VlJnu5Cx7ajpkbtgVik"></img>
+          <div>
+            <h1>Unity Software-Engineer 2021 - Current</h1>
+            <p>Working on simulations @BlueTea</p>
+          </div>
+        </div>
+
+        <div className='ExperienceBlock'>
+          <img src="https://media.licdn.com/dms/image/C510BAQHRl2VJ0_8qZw/company-logo_100_100/0/1519875113257?e=1686787200&v=beta&t=l-pb48QF9g_82IgrxVsWqBD7VlJnu5Cx7ajpkbtgVik"></img>
+          <div>
+            <h1>Unity Software-Engineer 2021 - Current</h1>
+            <p>Working on simulations @BlueTea</p>
+          </div>
+        </div>
+
+        <div className='ExperienceBlock'>
+          <img src="https://media.licdn.com/dms/image/C510BAQHRl2VJ0_8qZw/company-logo_100_100/0/1519875113257?e=1686787200&v=beta&t=l-pb48QF9g_82IgrxVsWqBD7VlJnu5Cx7ajpkbtgVik"></img>
+          <div>
+            <h1>Unity Software-Engineer 2021 - Current</h1>
+            <p>Working on simulations @BlueTea</p>
+          </div>
+        </div>
+        
         <FooterElement />
       </div>
     );

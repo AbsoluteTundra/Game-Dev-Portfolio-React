9c005eb530b93a75808260fec104f8eb504fd1a3
AbsoluteTundra <michelvanderlinden@live.nl>
2024-07-11 14:57:13 +0200

    Update ProjectScreenShotGalleryElement.js

--- src/ProjectScreenShotGalleryElement.js
+++ src/ProjectScreenShotGalleryElement.js
@@ -26,8 +26,6 @@ class ProjectScreenShotGalleryElement extends Component {
         });
     };
 
-
-
     render() {
         return (
             <div className="ProjectScreenshotGalleryContainer">

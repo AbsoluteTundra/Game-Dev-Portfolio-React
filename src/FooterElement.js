a7d3f9b10d899bd3516c6fae810ec2354740b5a9
AbsoluteTundra <michelvanderlinden@live.nl>
2024-07-12 21:30:00 +0200

    Add more links to the footer

--- src/FooterElement.js
+++ src/FooterElement.js
@@ -13,6 +13,8 @@ class FooterElement extends Component
         return(
             <footer>
                <a href="https://www.linkedin.com/in/michel-van-der-linden-0796315b/"><img className="FooterIcon" src="linkedin_logo.png"/></a>
+               <a href="https://github.com/AbsoluteTundra"><img className="FooterIcon" src="github-mark-white.png"/></a>
+               <a href="https://itch.io/profile/absolutetundragames"><img className="FooterIcon" src="itchio-logo-textless-white.png"/></a>
                </footer>
         );
     }

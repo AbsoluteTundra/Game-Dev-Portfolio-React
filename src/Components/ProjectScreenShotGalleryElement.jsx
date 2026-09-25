97e92b87e35cce88f899dc7ba903c85f956bd2e1
AbsoluteTundra <michelvanderlinden@live.nl>
2026-09-25 07:57:26 +0200

    [feature][screenshot gallery] - option to click through the screenshots

--- src/Components/ProjectScreenShotGalleryElement.jsx
+++ src/Components/ProjectScreenShotGalleryElement.jsx
@@ -9,6 +9,11 @@ class ProjectScreenShotGalleryElement extends Component {
     }
 
     componentDidMount() {
+        this.startInterval();
+    }
+
+    startInterval() {
+        clearInterval(this.interval);
         this.interval = setInterval(this.NextScreenShot, this.props.intervalTime == undefined ? 5000 : this.props.intervalTime);
     }
 
@@ -18,16 +23,31 @@ class ProjectScreenShotGalleryElement extends Component {
 
     NextScreenShot = () => {
         this.setState((prevState) => {
+            this.startInterval();
             const nextIndex = prevState.currentImageIndex + 1 > this.props.screenshotCount ? 1 : prevState.currentImageIndex + 1;
             return { currentImageIndex: nextIndex };
         });
     };
 
+    PreviousScreenShot = () => {
+        this.startInterval();
+        this.setState((prevState) => {
+            const nextIndex = prevState.currentImageIndex - 1 < 1 ? this.props.screenshotCount : prevState.currentImageIndex - 1;
+            return { currentImageIndex: nextIndex };
+        });
+    }
+
     render() {
         return (
             <div className="ProjectScreenshotGalleryContainer">
                 <h1>Screenshots</h1>
-                <img className="ProjectScreenshotGallery" src={`${this.props.screenshotsDirectory}screenshot${this.state.currentImageIndex}.jpg`}></img>
+                <div className="ProjectScreenshotGallery">
+                    <svg onClick={this.PreviousScreenShot} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="arrow arrow-left"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5"/>
+                    </svg>
+                    <img src={`${this.props.screenshotsDirectory}screenshot${this.state.currentImageIndex}.jpg`}></img>
+                    <svg onClick={this.NextScreenShot} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="arrow arrow-right"><path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
+                    </svg>
+                </div>
             </div>
         );
     }
